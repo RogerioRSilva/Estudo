d1eb5649ebc995273c98add10532e7144e31bc32
RogerioRSilva <114150848+RogerioRSilva@users.noreply.github.com>
2022-09-29 16:52:03 -0300

    Create Operadores.js

---
+++ JS/JS Curso em Vídeo/Aula07/Operadores.js
@@ -0,0 +1,23 @@
+/* ++++++++++++ Oeprações em JS +++++++++++++++
+Os operadores são esses:
++ =  Soma e concatenação de string
+- = Subtração
+* = Multiplicação
+/ = Divisão
+% = Modulo ou resto da divisão inteiro
+** = Potenciação
+*/
+
+var soma = 10 + 20
+var sub = 10 - 20
+var mult = 10 * 20
+var div = 10 / 2
+var mod = 10 % 8
+var pot = 10 ** 2
+
+console.log(`Soma 10 + 20: ${soma}`)
+console.log(`Subitração 10 - 20: ${sub}`)
+console.log(`Multiplicação 10 * 20: ${mult}`)
+console.log(`Divisão 10 / 2: ${div}`)
+console.log(`Modulo 10 % 8: ${mod}`)
+console.log(`Potenciação 10²: ${pot}`)
