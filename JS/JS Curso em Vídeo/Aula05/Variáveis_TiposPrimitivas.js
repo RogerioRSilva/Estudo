39eb7af360c86987b515a7aa0ee1ac12207549bb
RogerioRSilva <114150848+RogerioRSilva@users.noreply.github.com>
2022-09-29 14:42:02 -0300

    Create Variáveis_TiposPrimitivas.js

---
+++ JS/JS Curso em Vídeo/Aula05/Variáveis_TiposPrimitivas.js
@@ -0,0 +1,12 @@
+//Variáveis: 
+//Numericas = integer, float
+//Textos = string, str
+//Booleanas: False, True
+
+var rog = 'Rogério'
+var num = 100
+var bol = true
+
+console.log(typeof (rog))
+console.log(typeof (num))
+console.log(typeof (bol))
