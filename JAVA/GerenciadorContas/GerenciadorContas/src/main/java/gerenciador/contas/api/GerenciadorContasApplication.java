ab0878508882e4e96dd2ca4ff89d1f0a42e0e2d2
RogerioRSilva <rogrodriguessilva2022@gmail.com>
2024-01-02 17:38:18 -0300

    Aplicação de gerenciamento de contas

---
+++ JAVA/GerenciadorContas/GerenciadorContas/src/main/java/gerenciador/contas/api/GerenciadorContasApplication.java
@@ -0,0 +1,13 @@
+package gerenciador.contas.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorContasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorContasApplication.class, args);
+	}
+
+}
---
+++ JAVA/GerenciadorContas/GerenciadorContas/src/main/java/gerenciador/contas/api/config/CorsConfiguration.java
@@ -0,0 +1,16 @@
+package gerenciador.contas.api.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    // Recurso utilizado para consumir a API de uma aplicação FRONT-END
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+    }
+}
